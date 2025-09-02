eb7091ccb1b77f3fa9f8537aedd1ec00016846ff
romankhan5503 <romankhan5503@gmail.com>
2025-09-02 21:20:36 +0530

    anything

--- src/loops/Forloop.java
+++ src/loops/Forloop.java
@@ -3,42 +3,69 @@ package loops;
 public class Forloop {
 
 	public static void main(String[] args) {
+		/*
 		for (int i=0; i<=5; i++) {
 			System.out.println(i);
 		}
+		*/
 		
+         /*
 		//for even number
-		for (int i = 2; i<=20; i=i+2) {
+		for (int i = 2; i<=50; i=i+2) {
 			System.out.println(i);
-		}
+	}
+		*/
 		
-		
-		
+		/*
 		//for sum of numbers
 		int sum = 0;
-		for (int i=1; i<=5; i++) {
-			sum = sum + i;
+		for (int i=1; i<=100; i++) {
+			sum += i;
 		}
-		System.out.println(sum);
+		System.out.println("Sum = " +sum);
+		*/
 		
-		
-		
+		/*
 		//Enhanced for loop
 		int number [] = {1,2,3,4,5,6,7,8,9};
 		
 		for (int i: number) {
 			System.out.println(i);
 		}
+		*/
 		
-		
+		/*
 		//real life example
 		for (int i = 3; i <= 30; i+=3) {
 			System.out.println(i);
 		}
+		*/
 		/*When you know exactly how many times you want to loop through a block of code, use the for loop instead of a while loop
 		 */
 		 
-
+		/*
+		for(int i =2; i<=50; i+=2) {
+			System.out.println(i);
+		}
+		*/
+		
+		/*
+		for(int i = 1; i<=50; i+=2) {
+			System.out.println(i);
+		}
+		*/
+		
+		int sum = 0;
+		for(int i = 1; i<=100; i++) {
+			sum+=i;
+		}
+		System.out.println(sum);
+		
+		/*
+		for (int i = 1; i<=10; i++) {
+			System.out.println(5*i);
+		}
+		*/
 	}
 
 }
--- src/variables/Student.java
+++ src/variables/Student.java
@@ -1,13 +1,30 @@
 package variables;
 
 public class Student {
-int x=10;
-int d=23;
- static int y=20;
-public static void main(String[] args) {
-	Student std= new Student();
-	System.out.println(std.x);
-	System.out.println(y);
+
+	 int number = 80;  //instance variable
+	 static int number2 = 80;  //instance variable
 	
+	void local(){
+		int num = 10;  //local variable
+		System.out.println(number + num);
+	}
+	
+	void local2() {
+		int num2 = 20;    //local variable
+		System.out.println(number + num2);
+	}
+	
+	static void local3() {
+		int num3 = 30;
+		System.out.println(number2 + num3);
+	}
+	
+public static void main(String[] args) {
+	Student s = new Student();
+	s.local();
+	s.local2();
+	//s.local3();
+	Student.local3();
 }
 }
